8207dfa27ad8416079f7edef0b7a6b991dfb5737
Tim Taylor <tjtnew@gmail.com>
2020-04-15 20:49:26 +0100

    Made project_beds the only user-facing function
    Made simulate_occupancy an internal function leaving project_beds
    as the only exported function.  Moved sanity checks from
    simulate_occupancy to project_beds.
    
    Also rexported the build_projections function so we could have a
    documentation link in the pkgdown site.

--- R/project_beds.R
+++ R/project_beds.R
@@ -1,21 +1,100 @@
-#' Project bed occupancy from admissions
+#' @importFrom methods is
+NULL
+
+#' @importFrom projections build_projections
+NULL
+
+
+#' @title Constructor for projections objects
+#' @keywords NULL
+#' @export
+#' @name build_projections
+projections::build_projections
+
+#' Simulator for projecting bed occupancy
 #'
-#' This projects bed occupancy using admission incidence stored as a
-#' `projections` object, and a distribution of length of stay (los). This is a
-#' wrapper around `simulate_occupancy`, which essentially applies this function
-#' to different admission trajectories and collects outputs into a single
-#' `projections` object.
+#' This function predits bed occupancy from admission data (dates, and numbers
+#' of admissions on these days). Duration of hospitalisation is provided by a
+#' function returning `integer` values for the number of days in hospital.
 #'
-#' @param x a `projections` object storing forecast of daily admissions
+#' @param dates A vector of dates, ideally as `Date` but `integer` should work too.
 #'
-#' @param r_los function generating random duration of hospitalisation
+#' @param n_admissions An `integer` vector giving the number of admissions
+#'   predicted for each date in `dates`.
+#'
+#' @param r_los A `function` with a single parameter `n` returning `n` `integer`
+#'   values of lenth of hospital stay (LoS) in days. Ideally, this should come
+#'   from a discrete random distribution, such as `rexp` or any `distcrete`
+#'   object.
 #'
 #' @param n_sim The number of times duration of hospitalisation is simulated for
 #'   each admission. Defaults to 10. Only relevant for low (<30) numbers of
 #'   initial admissions, in which case it helps accounting for the uncertainty
 #'   in LoS.
 #'
-#' @seealso \code{\link{simulate_occupancy}}
+#' @author Thibaut Jombart
+#' @keywords internal
+simulate_occupancy <- function(n_admissions, dates, r_los, n_sim = 10) {
+
+    ## Outline:
+
+    ## We take a vector of dates and incidence of admissions, and turn this into a
+    ## vector of admission dates, whose length is sum(n_admissions). We will
+    ## simulate for each date of admission a duration of stay, and a corresponding
+    ## vector of dates at which this case occupies a bed. Used beds are then
+    ## counted (summing up all cases) for each day. To account for stochasticity
+    ## in duration of stay, this process can be replicated `n_sim` times,
+    ## resulting in `n_sim` predictions of bed needs over time.
+
+
+    admission_dates <- rep(dates, n_admissions)
+    n <- length(admission_dates)
+    last_date <- max(dates)
+    out <- vector(n_sim, mode = "list")
+
+
+    for (j in seq_len(n_sim)) {
+        los <- r_los(n)
+        list_dates_beds <- lapply(seq_len(n),
+                                  function(i) seq(admission_dates[i],
+                                                  length.out = los[i],
+                                                  by = 1L))
+        ## Note: unlist() doesn't work with Date objects
+        dates_beds <- do.call(c, list_dates_beds)
+        dates_beds <- dates_beds[dates_beds <= last_date]
+        beds_days <- incidence::incidence(dates_beds)
+
+        out[[j]] <- projections::build_projections(
+            x = beds_days$counts,
+            dates = incidence::get_dates(beds_days))
+    }
+
+    projections::merge_projections(out)
+
+}
+
+
+#' Project bed occupancy from admissions
+#'
+#' This function projects bed occupancy using admission incidence and a
+#' distribution of length of stay (los).
+#'
+#' @param x a \code{\link[projections:build_projections]{projections}} object
+#' storing one or more integer forecasts of daily admissions.
+#'
+#' @param r_los A `function` with a single parameter `n` returning `n` `integer`
+#'   values of lenth of hospital stay (LoS) in days. Ideally, this should come
+#'   from a discrete random distribution, such as `rexp` or any `distcrete`
+#'   object.
+#'
+#' @param n_sim The number of times duration of hospitalisation is simulated for
+#'   each admission. Defaults to 10. Only relevant for low (<30) numbers of
+#'   initial admissions, in which case it helps accounting for the uncertainty
+#'   in LoS.
+#'
+#'  @return A \code{\link[projections:build_projections]{projections}} object
+#'  that collects the output from the different admission trajectories.
+#'
 #'
 #' @examples
 #' if (require(projections))  {
@@ -40,7 +119,21 @@
 #' }
 #'
 #' @export
-project_beds <- function(x, r_los, n_sim = 1) {
+project_beds <- function(x, r_los, n_sim = 10) {
+
+    ## sanity checks
+    if (!all(is.finite(x))) stop("projection in x contains a non-numeric value")
+
+    if (!all(x > 0)) stop("projected values in x must be >= 1")
+
+    if (!is.finite(n_sim)) stop("`n_sim` is not a number")
+
+    if (n_sim[1] < 1) stop("`n_sim` must be >= 1")
+
+    if (inherits(r_los, "distcrete")) {
+        r_los <- r_los$r
+    }
+    if (!is.function(r_los)) stop("`r_los` must be a function")
 
     ## get daily bed needs predictions for each simulated trajectory of admissions
     x_dates <- projections::get_dates(x)
---
+++ tests/testthat/test-project_beds.R
@@ -0,0 +1,50 @@
+test_that("project_beds - test 1", {
+
+    # parameters
+    dates <- Sys.Date() - 1:10
+    admissions <- rep(1, 10)
+    rlos <- function(n) {
+        rep(1, n)
+    }
+
+    # setup
+    x = projections::build_projections(x = admissions, dates = dates)
+
+    # simulation results
+    beds <- project_beds(x, rlos, n_sim = 10)
+
+    # check daily occupancies
+    expected <- matrix(1, 10, 10)
+    expect_true(all(expected == beds))
+
+    # check rownames
+    beds_dates = rownames(beds)
+    expected = as.character(sort(dates))
+    expect_equal(beds_dates, expected)
+})
+
+## contant admissions and long length of stay
+test_that("project_beds - test 2", {
+
+    # parameters
+    dates <- Sys.Date() - 1:10
+    admissions <- rep(1, 10)
+    rlos <- function(n) {
+        rep(10, n)
+    }
+
+    # setup
+    x = projections::build_projections(x = admissions, dates = dates)
+
+    # simulation results
+    beds <- project_beds(x, rlos, n_sim = 10)
+
+    # check daily occupancies
+    expected <- matrix(1:10, nrow = 10, ncol = 10)
+    expect_true(all(expected == beds))
+
+    # check rownames
+    beds_dates = rownames(beds)
+    expected = as.character(sort(dates))
+    expect_equal(beds_dates, expected)
+})
