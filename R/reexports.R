b0178652c8b88bb0a0cc554bbe84ad93976dacbe
Tim Taylor <tjtnew@gmail.com>
2020-04-27 20:15:50 +0100

    removed unused function and renamed to make clear what functions are for

---
+++ R/reexports.R
@@ -0,0 +1,9 @@
+#' @title Constructor for projections objects
+#' @importFrom projections build_projections
+#' @export
+projections::build_projections
+
+#' @title Compute incidence of events from a vector of dates
+#' @importFrom incidence incidence
+#' @export
+incidence::incidence
