b6efb89efd5ad1170b5f22d0ab5864c0141920ab
Tim Taylor <tjtnew@gmail.com>
2020-04-27 20:32:28 +0100

    added @name tags to ensure separate docs

--- R/reexports.R
+++ R/reexports.R
@@ -1,9 +1,11 @@
 #' @title Constructor for projections objects
 #' @importFrom projections build_projections
 #' @export
+#' @name build_projections
 projections::build_projections
 
 #' @title Compute incidence of events from a vector of dates
 #' @importFrom incidence incidence
 #' @export
+#' @name incidence
 incidence::incidence
